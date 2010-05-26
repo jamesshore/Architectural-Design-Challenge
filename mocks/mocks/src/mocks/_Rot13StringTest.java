b874d9f8241606107dea8bceb466f5f39152e63d
James Shore <jshore@jamesshore.com>
2010-05-25 22:18:14 -0700

    Started Rot13String.transform()

--- mocks/mocks/src/mocks/_Rot13StringTest.java
+++ mocks/mocks/src/mocks/_Rot13StringTest.java
@@ -8,7 +8,19 @@ import org.junit.Test;
 public class _Rot13StringTest {
 
 	@Test
-	public void nothing() {
-		fail("foo");
+	public void transform() {
+		Rot13String string = new Rot13String("abc");
+		assertEquals("transformed string", "mno", string.transform());
+	}
+	
+	@Test
+	public void equalsAndHashCode() {
+		Rot13String string1a = new Rot13String("abc");
+		Rot13String string1b = new Rot13String("abc");
+		Rot13String string2 = new Rot13String("def");
+		
+		assertEquals(string1a, string1b);
+		assertFalse(string1a.equals(string2));
+		assertEquals(string1a.hashCode(), string1b.hashCode());
 	}
 }
