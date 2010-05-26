436f1e9a3e44c4e417d1608d2d76a7becb90ab56
James Shore <jshore@jamesshore.com>
2010-05-26 12:05:20 -0700

    Good stopping point

---
+++ mocks/mocks/src/mocks/Rot13StringFactory.java
@@ -0,0 +1,17 @@
+package mocks;
+
+import java.io.IOException;
+
+public class Rot13StringFactory {
+
+	private FileSystem _fileSystem;
+	
+	public Rot13StringFactory(FileSystem fileSystem) {
+		_fileSystem = fileSystem;
+	}
+
+	public Rot13String createFromFile(String filename) throws IOException {
+		//return new Rot13String(_fileSystem.readFile(filename));
+	}
+
+}
---
+++ mocks/mocks/src/mocks/_Rot13StringFactoryTest.java
@@ -0,0 +1,30 @@
+package mocks;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JMock.class)
+public class _Rot13StringFactoryTest {
+	private Mockery _mockery = new JUnit4Mockery();
+
+	@Test
+	public void createFromFile() throws IOException {
+		final FileSystem fileSystem = _mockery.mock(FileSystem.class);
+		Rot13StringFactory factory = new Rot13StringFactory(fileSystem);
+		
+		_mockery.checking(new Expectations() {{
+			oneOf (fileSystem).readFile("filename"); will(returnValue("abc"));
+		}});
+
+		Rot13String string = factory.createFromFile("filename");
+		assertEquals(new Rot13String("abc"), string);
+	}
+}
