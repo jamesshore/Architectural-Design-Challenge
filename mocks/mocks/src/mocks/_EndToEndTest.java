baff8ff5b6704fe511aa67d5e32278da92750471
James Shore <jshore@jamesshore.com>
2010-05-29 15:58:09 -0700

    Cleaned up _EndToEndTest

--- mocks/mocks/src/mocks/_EndToEndTest.java
+++ mocks/mocks/src/mocks/_EndToEndTest.java
@@ -7,31 +7,33 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import org.junit.After;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
 public class _EndToEndTest {
-
 	private FileSystem _fileSystem = new FileSystemImpl();
+	private String inputFile = "in.txt";
+	private String outputFile = "out.txt";
+	
+	@After
+	public void teardown() {
+		if (_fileSystem.fileExists(inputFile)) _fileSystem.deleteFile(inputFile);
+		if (_fileSystem.fileExists(outputFile)) _fileSystem.deleteFile(outputFile);		
+	}
 	
 	@Test
 	public void endToEnd() throws IOException {
-		String inputFile = "in.txt";
-		String outputFile = "out.txt";
-		
 		String inputText = "The dog barks at midnight.";
+		_fileSystem.writeFile(inputFile, inputText);
+		
 		String expectedOutput = "Gur qbt onexf ng zvqavtug.";
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try {
-			_fileSystem.writeFile(inputFile, inputText);
-			UI.main(new String[] { inputFile, outputFile }, new PrintStream(out));
-			assertEquals(expectedOutput, _fileSystem.readFile(outputFile));
-			assertEquals(expectedOutput, out.toString());
-		}
-		finally {
-			if (_fileSystem.fileExists(inputFile)) _fileSystem.deleteFile(inputFile);
-			if (_fileSystem.fileExists(outputFile)) _fileSystem.deleteFile(outputFile);
-		}
+		ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
+		
+		UI.main(new String[] { inputFile, outputFile }, new PrintStream(consoleOutput));
+		assertEquals(expectedOutput, _fileSystem.readFile(outputFile));
+		assertEquals(expectedOutput, consoleOutput.toString());
 	}
 	
 	@Test
