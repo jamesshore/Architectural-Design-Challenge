e6673143416a648289b2fa8b2fdee5225a092794
James Shore <jshore@jamesshore.com>
2010-05-29 17:52:31 -0700

    Name tweaks.

--- mocks/mocks/src/mocks/__EndToEndTest.java
+++ mocks/mocks/src/mocks/__EndToEndTest.java
@@ -8,28 +8,28 @@ import org.junit.After;
 import org.junit.Test;
 
 
-public class _EndToEndTest {
+public class __EndToEndTest {
 	private Configuration _configuration = Configuration.test();
 	private FileSystem _fileSystem = new FileSystemImpl(Configuration.test());
-	private String inputFile = "in.txt";
-	private String outputFile = "out.txt";
+	private String _inputFile = "in.txt";
+	private String _outputFile = "out.txt";
 	
 	@After
 	public void teardown() {
-		_fileSystem.deleteFile(inputFile);
-		_fileSystem.deleteFile(outputFile);		
+		_fileSystem.deleteFile(_inputFile);
+		_fileSystem.deleteFile(_outputFile);		
 	}
 	
 	@Test
 	public void endToEnd() throws IOException {
 		String inputText = "The dog barks at midnight.";
-		_fileSystem.writeFile(inputFile, inputText);
+		_fileSystem.writeFile(_inputFile, inputText);
 		
 		String expectedOutput = "Gur qbt onexf ng zvqavtug.";
 		ByteArrayOutputStream consoleOutput = new ByteArrayOutputStream();
 		
-		UI.main(new String[] { inputFile, outputFile }, new PrintStream(consoleOutput), _configuration);
-		assertEquals(expectedOutput, _fileSystem.readFile(outputFile));
+		UI.main(new String[] { _inputFile, _outputFile }, new PrintStream(consoleOutput), _configuration);
+		assertEquals(expectedOutput, _fileSystem.readFile(_outputFile));
 		assertEquals(expectedOutput, consoleOutput.toString());
 	}
 }
